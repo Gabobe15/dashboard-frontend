9506f1d5993d701bd516e5be5dbc4c2ac322a0ab
Gabobe15 <gabobe33@gmail.com>
2024-01-15 12:17:24 +0300

    middleware.ts file but not working

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@mui/material";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { Button } from '@mui/material';
+import { useSession, signIn, signOut } from 'next-auth/react';
 export default function Component() {
   const { data: session } = useSession();
   if (session) {
     return (
       <>
-        <Button variant="contained" color={"error"} onClick={() => signOut()}>
+        <Button variant="contained" color={'error'} onClick={() => signOut()}>
           Sign out
         </Button>
       </>
@@ -13,8 +13,7 @@ export default function Component() {
   }
   return (
     <>
-      <h2>Please log in</h2>
-      <Button variant={"contained"} color={"success"} onClick={() => signIn()}>
+      <Button variant={'contained'} color={'success'} onClick={() => signIn()}>
         Sign in
       </Button>
     </>
